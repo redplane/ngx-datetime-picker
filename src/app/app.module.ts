acfa9709cbc11f0a85084ca7c4e1d27b2264a73d
Linh Nguyem <redplane_dt@yahoo.com.vn>
2017-06-23 22:42:45 +0700

    - Update app code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import {NgxDaySelectorComponent} from "modules/ngx-bootstrap-datetime-picker/components/ngx-day-selector.component";
-import {NgxMonthSelectorComponent} from "modules/ngx-bootstrap-datetime-picker/components/ngx-month-selector.component";
-import {NgxYearSelectorComponent} from "modules/ngx-bootstrap-datetime-picker/components/ngx-year-selector.component";
-import {NgxTimeSelectorComponent} from "modules/ngx-bootstrap-datetime-picker/components/ngx-time-selector.component";
-import {NgxBootstrapDateTimePickerComponent} from "modules/ngx-bootstrap-datetime-picker/ngx-bootstrap-datetime-picker.component";
-import {NgxNumericDateTimePickerComponent} from "modules/ngx-numeric-datetime-picker/ngx-numeric-datetime-picker.component";
-
+import {NgxBootstrapDateTimePickerModule} from "../modules/ngx-bootstrap-datetime-picker/ngx-bootstrap-datetime-picker.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NgxBootstrapDateTimePickerComponent,
-    NgxNumericDateTimePickerComponent,
-    NgxDaySelectorComponent,
-    NgxMonthSelectorComponent,
-    NgxYearSelectorComponent,
-    NgxTimeSelectorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    NgxBootstrapDateTimePickerModule
   ],
   providers: [
 
